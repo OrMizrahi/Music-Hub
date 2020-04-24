c522b33fe4ed52286a88c8697509d53a4395da5f
or mizrahi <ormizrahi777@gmail.com>
2020-04-24 14:50:55 +0300

    37

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+module.exports = {
+	googleClientID:
+		'512451103985-7lrh8e2c5d5428qmg8fbjlgi7191hevr.apps.googleusercontent.com',
+	googleClientSecret: '23EN23bDS1rU95Gqw7Wnk9SV',
+	facebookClientID: '559400024710355',
+	facebookClientSecret: '9492e687ac5ca5f8cee2e16817b70248',
+	twitterClientID: 'rXi1UJaRZ7s1iFNxDJMnJIlGI',
+	twitterClientSecret: 'JTSSWUsyZ3ywFkBi0PrOzM88pCnOmCHbg9uV83HIro9rZB5lk4',
+	mongoURI:
+		'mongodb+srv://OrMizrahi:Fi2gHkK8VVWIuBbo@my-mern-app-cymvz.mongodb.net/test?retryWrites=true&w=majority',
+	cookieKey: 'jsfgbhehfkjwehriwenjkfawhqeiuqbewfugkvbsukv',
+};
