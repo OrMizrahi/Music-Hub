9198df45778091957b43fd93926808560c9cc270
or mizrahi <ormizrahi777@gmail.com>
2020-04-26 13:18:38 +0300

    added voice recognition- for future

--- client/src/components/vsd.js
+++ client/src/components/vsd.js
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
-import { useSpeechRecognition } from 'react-speech-kit';
+// import React, { useState } from 'react';
+// import { useSpeechRecognition } from 'react-speech-kit';
 
-function Example() {
-	const [value, setValue] = useState('');
-	const { listen, listening, stop } = useSpeechRecognition({
-		onResult: (result) => {
-			setValue(result);
-		},
-	});
+// function Example() {
+// 	const [value, setValue] = useState('');
+// 	const { listen, listening, stop } = useSpeechRecognition({
+// 		onResult: (result) => {
+// 			setValue(result);
+// 		},
+// 	});
 
-	return (
-		<div>
-			<textarea
-				value={value}
-				onChange={(event) => setValue(event.target.value)}
-			/>
-			<button onMouseDown={listen} onMouseUp={stop}>
-				🎤
-			</button>
-			{listening && <div>Go ahead I'm listening</div>}
-		</div>
-	);
-}
+// 	return (
+// 		<div>
+// 			<textarea
+// 				value={value}
+// 				onChange={(event) => setValue(event.target.value)}
+// 			/>
+// 			<button onMouseDown={listen} onMouseUp={stop}>
+// 				🎤
+// 			</button>
+// 			{listening && <div>Go ahead I'm listening</div>}
+// 		</div>
+// 	);
+// }
 
-export default Example;
+// export default Example;
